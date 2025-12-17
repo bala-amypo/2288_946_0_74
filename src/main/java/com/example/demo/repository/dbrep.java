bb4480f24565e8c05944036512de9cbd911a2626
bala-amypo <bala@amypo.in>
2025-12-17 06:31:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/dbrep.java
+++ src/main/java/com/example/demo/repository/dbrep.java
@@ -1,9 +1,9 @@
-package com.example.demo.Repository;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.dben;
-
+@Repository
 public interface dbrep extends JpaRepository<dben, Integer> {
     
 
